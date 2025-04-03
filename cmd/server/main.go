8ec791c2c03ea9ea12114044b6a2500e96d51f90
Eric <erics1337@gmail.com>
2025-04-03 10:06:55 -0600

    Add RabbitMQ connection and graceful shutdown to server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const rabbitMQConnString = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	fmt.Println("Waiting for RabbitMQ to start...")
+	time.Sleep(7 * time.Second) // Add a delay to prevent race condition from container startup time
+
+	conn, err := amqp.Dial(rabbitMQConnString)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Successfully connected to RabbitMQ")
+	fmt.Println("Waiting for messages... To exit press CTRL+C")
+
+	// Block until a signal is received
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down...")
 }
